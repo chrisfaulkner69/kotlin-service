b06a28d363a66d16cdbbdee97b3d4f42d3154845
Chris Faulkner <chris.faulkner@gmail.com>
2025-03-03 12:12:52 +0000

    revert to Java 17

--- src/main/kotlin/org/lucidant/kotlinservice/controller/PingController.kt
+++ src/main/kotlin/org/lucidant/kotlinservice/controller/PingController.kt
@@ -1,19 +1,18 @@
 package org.lucidant.kotlinservice.controller
 
-import io.swagger.annotations.Api
-import io.swagger.annotations.ApiOperation
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.lucidant.kotlinservice.configuration.SampleProperties
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 
-@Api(value = "Ping")
+@Tag(name = "Ping")
 @RestController
 class PingController(val sampleProperties: SampleProperties) {
 
-
-  @ApiOperation(value = "Endpoint to test status of server")
+  @Operation(summary = "Endpoint to test status of server")
   @CrossOrigin
   @RequestMapping(value = ["/ping"], method = [RequestMethod.GET], produces = ["text/plain"])
   fun ping(): String {
